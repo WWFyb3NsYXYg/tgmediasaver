bcab90c5eb884e01c5b0a168c1297f1d09c1890f
Ya <87089735+WWFyb3NsYXYg@users.noreply.github.com>
2023-06-07 23:30:39 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+api_id = 'YOUR_API_ID'
+api_hash = 'YOUR_API_HASH'
+entity = YOUR_UID_OR_CHAT_OR_CHANNEL_ID
--- main.py
+++ main.py
@@ -1,38 +1,31 @@
 from telethon import TelegramClient, events
+import config
 
-api_id = 'API-ID'
-api_hash = 'API-HASH'
-entity = CHAT_ID
-
-client = TelegramClient('bot_session', api_id, api_hash)
+client = TelegramClient('bot_session', config.api_id, config.api_hash)
 
 
-@client.on(events.NewMessage(outgoing = False) )
+@client.on(events.NewMessage(outgoing=False))
 @client.on(events.Album)
 async def handle_new_message(event):
-    execute_handle_new_message = True
+    sender = await event.get_sender()
+    user_id = sender.id
     if event.grouped_id:
-        try:
-            execute_handle_new_message = False
-            sent_message = await event.forward_to(entity)
-            reply_text = f'#album, #{event.chat_id}'
-            await client.send_message(entity, reply_text, reply_to = sent_message[len(event)-1].id)
-        except Exception as e:
-            if event.media:
-                media_type = None
+        print('Got an album with', len(event), 'items')
+        sent_message = await event.forward_to(config.entity)
+        reply_text = f'TAG: #album\nUID: ```{user_id}```'
+        await client.send_message(config.entity, reply_text, reply_to=sent_message[len(event)-1].id, parse_mode="Markdown")
+    if event.media:
+        media_type = None
 
-                if hasattr(event.media, 'photo'):
-                    media_type = 'photo'
+        if hasattr(event.media, 'photo'):
+            media_type = 'photo'
 
-                elif hasattr(event.media, 'document'):
-                    media_type = 'document'
+        elif hasattr(event.media, 'document'):
+            media_type = 'document'
 
-                reply_text = f'#{media_type}, #{event.chat_id}'
-                sent_message = await client.forward_messages(entity, event.message)
-            await client.send_message(entity, reply_text, reply_to = sent_message.id)
-
-
-
+        reply_text = f'TAG: #{media_type}\nUID: ```{user_id}```'
+        sent_message = await client.forward_messages(config.entity, event.message)
+        await client.send_message(config.entity, reply_text, reply_to=sent_message.id, parse_mode="Markdown")
 
 client.start()
 client.run_until_disconnected()
