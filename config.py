32d2f3d3294dede11ea0c080a91e36172c945986
Ya <87089735+WWFyb3NsYXYg@users.noreply.github.com>
2023-06-11 03:01:07 +0300

    Changes media type handling, add create_tag func
    Changes in the new code compared to the previous code:
    
    1. The TelegramClient module is imported from telethon.sync, not telethon.
    2. The app_version parameter is added
    3. The create_tag function is now declared to improve modularity and code reuse
    4. The from_group variable has been added to the create_tag function, which stores information about whether the event is from a group or not.
    5. In the event handler functions (handle_new_message), the conditions func=lambda events: events.is_private or events.is_group have been added to process only private messages or messages from groups.
    Instead of event.media , the new code uses the following checks to determine the media type:
    	- event.photo for photos.
    	- event.video_note for video notes.
    	- event.video for video.
    	- event.voice for voice messages.
    6. In the event handler functions, the create_tag function is used to send a message with a tag and a link to the user to the specified channel.
    A try-finally block has been added to ensure that the client is disconnected upon completion.

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 api_id = 'YOUR_API_ID'
 api_hash = 'YOUR_API_HASH'
-entity = YOUR_UID_OR_CHAT_OR_CHANNEL_ID
+channel = "YOUR_UID_OR_CHAT_OR_CHANNEL_ID" # You can also paste a link here in the format: https://t.me/+jJnXXXXXXXXg5
+app_version = "v0.0.2"
--- main.py
+++ main.py
@@ -1,31 +1,63 @@
-from telethon import TelegramClient, events
+from telethon.sync import TelegramClient, events
 import config
 
-client = TelegramClient('bot_session', config.api_id, config.api_hash)
+client = TelegramClient('TG_Media_Saver', config.api_id,
+                        config.api_hash, app_version=config.app_version)
 
 
-@client.on(events.NewMessage(outgoing=False))
-@client.on(events.Album)
+async def create_tag(event, media_type, user_id, sent_message):
+    from_group = ''
+    if event.is_group:
+        from_group = ", #from_group"
+
+    reply_text = f'TAG: #{media_type} {from_group}\nUID:<a href="tg://user?id={user_id}">{user_id}</a>'
+    if event.grouped_id:
+        reply_to = sent_message[0].id
+
+    else:
+        reply_to = sent_message.id
+
+    await client.send_message(config.channel, reply_text, reply_to=reply_to, parse_mode="HTML")
+
+
+@client.on(events.NewMessage(outgoing=False, func=lambda events: events.is_private or events.is_group))
+@client.on(events.Album(func=lambda events: events.is_private or events.is_group))
 async def handle_new_message(event):
     sender = await event.get_sender()
+    me = await client.get_me()
     user_id = sender.id
-    if event.grouped_id:
-        print('Got an album with', len(event), 'items')
-        sent_message = await event.forward_to(config.entity)
-        reply_text = f'TAG: #album\nUID: ```{user_id}```'
-        await client.send_message(config.entity, reply_text, reply_to=sent_message[len(event)-1].id, parse_mode="Markdown")
-    if event.media:
-        media_type = None
+    if user_id != me.id:
+        if event.grouped_id:
+            # Do not touch this sacred seal, it causes an error due to which the code works as it should.
+            print('Got an album with', len(event), 'items')
+            media_type = 'album'
+            sent_message = await event.forward_to(config.channel)
+            await create_tag(event, media_type, user_id, sent_message)
 
-        if hasattr(event.media, 'photo'):
+        if event.photo:
             media_type = 'photo'
+            sent_message = await client.forward_messages(config.channel, event.message)
+            await create_tag(event, media_type, user_id, sent_message)
 
-        elif hasattr(event.media, 'document'):
-            media_type = 'document'
+        elif event.video_note:
+            media_type = 'circle'
+            sent_message = await client.forward_messages(config.channel, event.message)
+            await create_tag(event, media_type, user_id, sent_message)
 
-        reply_text = f'TAG: #{media_type}\nUID: ```{user_id}```'
-        sent_message = await client.forward_messages(config.entity, event.message)
-        await client.send_message(config.entity, reply_text, reply_to=sent_message.id, parse_mode="Markdown")
+        elif event.video:
+            media_type = 'video'
+            sent_message = await client.forward_messages(config.channel, event.message)
+            await create_tag(event, media_type, user_id, sent_message)
 
-client.start()
-client.run_until_disconnected()
+        elif event.voice:
+            media_type = 'voice'
+            sent_message = await client.forward_messages(config.channel, event.message)
+            await create_tag(event, media_type, user_id, sent_message)
+
+try:
+    print('(Successfully launched)')
+    print('(Press Ctrl+C to stop)')
+    client.start()
+    client.run_until_disconnected()
+finally:
+    client.disconnect()
